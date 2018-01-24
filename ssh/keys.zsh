414261b023879132926e71c84c82fd87e321a45c
Jonathan Hall <jlhall@23andme.com>
2018-01-24 08:49:51 -0800

    Some vim flavor stuff cause I'm a baby

--- ssh/keys.zsh
+++ ssh/keys.zsh
@@ -1,2 +1,2 @@
 # Pipe my public key to my clipboard.
-alias pubkey="more $ZSH/ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
+alias pubkey="more $ZSH/ssh/id_rsa_gh.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
