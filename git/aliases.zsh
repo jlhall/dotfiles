b5c04a4a37b3a9b94b5cb95bfbdf907c0fd7efa4
Jonathan Hall <jlhall@23andme.com>
2018-01-24 16:12:51 -0800

    Random updates

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,10 +9,9 @@ alias gf='git fetch --all --prune'
 alias ggf='git ls-files | grep'
 alias gg="git grep -I --ignore-case --line-number --full-name --color --break --heading --context 2 --show-function"
 alias gglog='git log --patch -G '
-alias gl='git pull'
+alias gp='git pull'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gme="glog --author=\"Jonathan Hall\""
-alias gp='git push origin HEAD --tags'
 alias greset="git reset --hard"
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
 alias gs='git status -sb'
